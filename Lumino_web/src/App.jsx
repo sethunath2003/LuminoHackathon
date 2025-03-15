a75392c3fe869178823ec601cd26285c5e0ed790
Sethunath A <148645477+sethunath2003@users.noreply.github.com>
2025-03-15 17:27:55 +0530

    temp commit

--- Lumino_web/src/App.jsx
+++ Lumino_web/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./assets/components/common/Header";
+import Footer from "./assets/components/common/Footer";
 import HomePage from "./pages/Homepage";
+import AboutUs from "./pages/AboutUs";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Blogpage from "./pages/Blogpage";
+import ContactPage from "./pages/ContactPage";
+import JoinUs from "./pages/JoinUs";
+import Esports from "./pages/Esports";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<div>About</div>} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/blog" element={<Blogpage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/join-us" element={<JoinUs />} />
+            <Route path="/esports" element={<Esports />} /> */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
