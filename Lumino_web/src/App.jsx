d5419ea27a409118937cc5d687434669a90080b8
Sethunath A <148645477+sethunath2003@users.noreply.github.com>
2025-03-15 15:55:08 +0530

    first commit

---
+++ Lumino_web/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <div className=""></div>
+      
+    </Router>
+  )
+}
+
+export default App
